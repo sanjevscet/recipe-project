7a4d9a7b8757ba6a74199f654d668c462069edbf
Sanjeev Kumar <Sanjeev.kumar@goavege.com>
2020-08-22 06:58:42 +0530

    chap009 done

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Recipe} from './recipe.model';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,9 +7,6 @@ import {Recipe} from './recipe.model';
 })
 export class RecipesComponent implements OnInit {
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-  ];
 
   constructor() { }
 
